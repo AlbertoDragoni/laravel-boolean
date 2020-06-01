cac60cc7b6d0c59c0804519de101ebfd76e3b577
Alberto Dragoni <albertodragoni@yahoo.it>
2020-06-01 16:06:32 +0200

    creazione scheletro grafico (2)

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -1,14 +1,19 @@
 @extends('layouts.app');
 @section('content')
-  <nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="#">Home</a></li>
-      <li class="breadcrumb-item active" aria-current="page">Pages</li>
-    </ol>
-  </nav>
+
   <div class="container">
     <div class="row">
-      <div class="col-8">
+      <div class="col-12">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Pages</li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-10">
         <div class="row">
           <div class="col-6">
             <h2>Pages</h2>
@@ -17,8 +22,31 @@
             <a href="#">Crea una pagina</a>
           </div>
         </div>
-        <table>
-
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Category</th>
+              <th>Tags</th>
+              <th colspan="3">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut</td>
+              <td>Miscellanea</td>
+              <td>Tag 1, Tag 2, Tag 3</td>
+              <td><a class="btn btn-primary" href="#">Modifica</a></td>
+              <td><a class="btn btn-info" href="#">Visualizza</a></td>
+              <td>
+                <form class="" action="index.html" method="post">
+                  <input class="btn btn-danger" type="submit" value="Elimina">
+                </form>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
